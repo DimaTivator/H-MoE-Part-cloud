daede1e5d4fb2efdacd9da5b8f7625dc77433b54
DimaTivator <andriianov164@gmail.com>
2026-08-11 19:30:44 +0400

    fix(stage4): allow AdEMAMix optimizer argument

--- third_party/Megatron-LM/megatron/training/arguments.py
+++ third_party/Megatron-LM/megatron/training/arguments.py
@@ -2565,7 +2565,7 @@ def _add_training_args(parser):
                        help='use FlashAttention implementation of attention. '
                        'https://arxiv.org/abs/2205.14135')
     group.add_argument('--optimizer', type=str, default='adam',
-                       choices=['adam', 'sgd', 'muon', 'dist_muon', 'lion', 'soap', 'adaptive_muon'],
+                       choices=['adam', 'sgd', 'muon', 'dist_muon', 'lion', 'soap', 'adaptive_muon', 'ademamix'],
                        help='Optimizer function. '
                             'Note: dist_muon is deprecated; use --optimizer muon '
                             'with --use-distributed-optimizer instead.')
